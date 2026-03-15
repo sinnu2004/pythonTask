db0e195350513bd888eb021d7ab8b8f02bf8958f
sinnu2004 <152984546+sinnu2004@users.noreply.github.com>
2026-03-15 10:30:57 +0530

    Question 3 Corrected

--- Task1/question3.py
+++ Task1/question3.py
@@ -4,6 +4,8 @@ a = int(input("Enter the 1st number :"))  # taking inputs
 b = int(input("Enter the 2nd number :"))
 print(a,b)
 
-a,b = b,a  # swapping to numbers 
+c = a
+a = b
+b = c# swapping to numbers 
 
 print(a,b)
